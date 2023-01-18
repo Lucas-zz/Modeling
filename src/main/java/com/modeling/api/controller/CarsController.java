ef10bbef11caea31ca877f3df7b75e301d736673
Lucas Azzolini Vieira <lucasazzollinivieira@hotmail.com>
2023-01-18 18:46:11 -0300

    feat: findAll and Save methods from repository added to controller

--- src/main/java/com/modeling/api/controller/CarsController.java
+++ src/main/java/com/modeling/api/controller/CarsController.java
@@ -1,5 +1,8 @@
 package com.modeling.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,19 +10,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.modeling.api.dto.CarDTO;
+import com.modeling.api.model.CarModel;
+import com.modeling.api.repository.CarRepository;
 
 @RestController
 @RequestMapping("/api/cars")
 public class CarsController {
 
+    @Autowired
+    private CarRepository repository;
+
     @GetMapping
-    public String getCars() {
-        return "List of cars";
+    public List<CarModel> getCars() {
+        return repository.findAll();
     }
 
     @PostMapping
-    public String createCar(@RequestBody CarDTO req) {
-        System.out.println(req.anoModelo());
-        return "Created";
+    public void createCar(@RequestBody CarDTO req) {
+        repository.save(new CarModel(req));
     }
 }
